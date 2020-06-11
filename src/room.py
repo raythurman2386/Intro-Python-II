f5ef227331d20cd46d338adc62f1a7eaad8a31ce
Ray Thurman <raymondthurman5@gmail.com>
2020-06-11 08:14:59 -0400

    updated room similar tho show directions in str

--- src/room.py
+++ src/room.py
@@ -6,11 +6,22 @@ class Room:
     def __init__(self, name, description):
         self.name = name
         self.description = description
+        self.n_to = None
+        self.s_to = None
+        self.e_to = None
+        self.w_to = None
         self.items = []
 
     def __str__(self):
-        output = f"\nWelcome to the {self.name}!"
-        output += f"\n\n{self.description}"
+        output = f"\n{self.name}: {self.description}\n"
+        if self.s_to:
+            output += f'To the south is: {self.s_to.name}\n'
+        if self.e_to:
+            output += f'To the east is: {self.e_to.name}\n'
+        if self.n_to:
+            output += f'To the north is: {self.n_to.name}\n'
+        if self.w_to:
+            output += f'To the west is: {self.w_to.name}\n'
         return output
 
     def show_items(self):
