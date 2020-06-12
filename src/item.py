5fa14a4356a7526e554fed6532b7d03f1288b9d3
Raymond Thurman <raymondthurman5@gmail.com>
2020-06-11 21:05:30 -0400

    added treasures and weapons

--- src/item.py
+++ src/item.py
@@ -7,15 +7,28 @@ class Item:
         return f"Item: {self.name}\nDesc: {self.description}"
 
 
+class Weapon(Item):
+    def __init__(self, name, description, damage, quality):
+        super().__init__(name, description)
+        self.damage = damage
+        self.quality = quality
+
+
+class Treasure(Item):
+    def __init__(self, name, description, value):
+        super().__init__(name, description)
+        self.value = value
+
+
 items = {
-    "Gold Coin": Item("Gold Coin", "Gold coin, looks like it could be of some value."),
+    "Gold Coin": Treasure("Gold Coin", "Gold coin, looks like it could be of some value.", "5 dolla"),
     "Health Potion": Item("Health Potion", "Hmmmmm, what could I possibly need this for??"),
     "Mana Potion": Item("Mana Potion", "Says on the bottle restores mana.... WTF is mana?"),
-    "Thunderfury": Item("Thunderfury", "Did you say: THUNDERFURY, BLESSED BLADE OF THE WINDSEEKER!"),
+    "Thunderfury": Weapon("Thunderfury", "Did you say: THUNDERFURY, BLESSED BLADE OF THE WINDSEEKER!", "over9000", "Legendary"),
     "Rusty Spoon": Item("Rusty Spoon", "Rusty spoon, looks like there is still some food on it."),
     "Pudding Cup": Item("Pudding Cup", "Who doesn't have time for pudding??"),
-    "Nerf Gun": Item("Nerf Gun", "You may want this to protect yourself!"),
+    "Nerf Gun": Weapon("Nerf Gun", "You may want this to protect yourself!", "Negative 4", "Plastic"),
     "Funko Pop": Item("Funko Pop", "Kinda looks like Goku"),
     "Water Fountain": Item("Water Fountain", "This could be handy, lets just put this in the back pocket."),
-    "Random Spaghetti": Item("Random Spaghetti", "No sauce.....Just noodles"),
+    "Random Spaghetti": Weapon("Random Spaghetti", "No sauce.....Just noodles", "One slimy point", "OODLES OF NOODLES"),
 }
